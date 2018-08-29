783e01742c268d7f32075e0501ab76df7d51d8bc
a215439057 <a215439057@qq.com>
2018-08-29 18:11:10 +0800

    git tracks changes

--- js/index.js
+++ js/index.js
@@ -48,6 +48,31 @@ window.onload = function(){
 		}
 
 	}
+            var Words = document.getElementById("words");
+            var Who = document.getElementById("who");
+            var TalkWords = document.getElementById("talkwords");
+            var TalkSub = document.getElementById("talksub");
+            TalkSub.onclick = function(){
+                //定义空字符串
+                var str = "";
+                var ste ="";
+                if(TalkWords.value == ""){
+                     // 消息为空时弹窗
+                    alert("Please enter content");
+                    return;
+                }
+                str = '<div class="btalk"><span>me:' + TalkWords.value +'</span></div>' ;  
+                if (TalkWords.value.indexOf("time") == 0) {
+                    	ste = '<div class="atalk"><span>me:' + '2 day' +'</span></div>'; 
+                    	
+					}		
+				else{
+					ste = '<div class="atalk"><span>me:' + 'no' +'</span></div>'; 
+				}	
+                //将之前的内容与要发的内容拼接好 提交
+                Words.innerHTML = Words.innerHTML + str +ste;
+            }
+        
 }
 
 window.onscroll=function()
