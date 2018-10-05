5ceacc388701cc07ce409a23a21687f137e414bc
a215439057 <a215439057@qq.com>
2018-10-05 16:31:15 +0800

    git tracks changes

--- js/index.js
+++ js/index.js
@@ -19,68 +19,52 @@ function c(){
 		var imag =1;
 		var imbg =2;
 		var imcg =3;
-		var ima =3;
-		var imb =2;
-		var imc =1;		
+		var news = document.querySelectorAll("#new_1");	
+
+
+		document.getElementById('next').onclick=function(){		
+					hehe();
+					for (var i = 0; i < news.length; i++) {
+						news[i].className ='iem';
+				}
+					news[imag-1].className='iem '+'iemt '+'lef '+'bock'+' abs'+' trans';
+					news[imbg-1].className='iem '+'iemt '+'bock'+' trans';
+					news[imcg-1].className='iem '+'righ '+'bock'+' abs';
+					haha();
+	}
+
 		document.getElementById('prev').onclick=function(){
-				var news = document.querySelectorAll("#new_1");	
-				for (var i = 0; i < news.length; i++) {
-					news[i].className ='iem';
-			}
-				news[imag-1].className='iem '+'iemt '+'lef '+'bock'+' abs';
-				news[imbg-1].className='iem '+'iemt '+'bock';
-				news[imcg-1].className='iem '+'righ '+'bock'+' abs';
-			if (imag == 3) {
-					imag = 1;	
+					hehe();
+					for (var i = 0; i < news.length; i++) {
+							news[i].className ='iem';
+					}
+					news[imag-1].className='iem '+'iemt '+'righ '+'bock'+' abs'+' trans';
+					news[imbg-1].className='iem '+'iemt '+'bock'+' trans';
+					news[imcg-1].className='iem '+'lef '+'bock'+' abs';
+					haha();
 				}
-			else{
-						
-					imag +=1;				
-				}
-			if (imbg == 3) {
-					imbg = 1;	
-				}
-			else{
-						
-					imbg +=1;				
-				}
-			if (imcg == 3) {
-					imcg = 1;	
-				}
-			else{
-						
-					imcg +=1;				
-				}	
-	}
-	
-	document.getElementById('next').onclick=function(){
-			var news = document.querySelectorAll("#new_1");
-			for (var i = 0; i < news.length; i++) {
-				news[i].className ='iem';
+		function hehe(){
+			
 		}
-			news[imcg-1].className='iem '+'iemt '+'lef '+'bock'+' abs';
-				news[imbg-1].className='iem '+'iemt '+'bock';
-				news[imag-1].className='iem '+'righ '+'bock'+' abs';
-			if (imag == 3) {
-					imag = 1;	
-				}
-			else{
-						
-					imag +=1;				
-				}
-			if (imbg == 3) {
-					imbg = 1;	
-				}
-			else{
-						
-					imbg +=1;				
-				}
-			if (imcg == 3) {
-					imcg = 1;	
-				}
-			else{
-						
-					imcg +=1;				
-				}	
+		function haha(){
+				if (imag == 3) {
+						imag = 1;	
+					}
+				else{
+						imag +=1;				
+					}
+				if (imbg == 3) {
+						imbg = 1;	
+					}
+				else{					
+						imbg +=1;				
+					}
+				if (imcg == 3) {
+						imcg = 1;	
+					}
+				else{					
+						imcg +=1;				
+					}	
+		}				
 	}
-}
+
