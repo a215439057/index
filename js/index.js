2f9a7571314c3742aec26aa1d8c49cbe26809f2d
a215439057 <a215439057@qq.com>
2018-10-08 13:52:29 +0800

    git tracks changes

--- js/index.js
+++ js/index.js
@@ -24,47 +24,59 @@ function c(){
 
 		document.getElementById('next').onclick=function(){		
 					hehe();
-					for (var i = 0; i < news.length; i++) {
-						news[i].className ='iem';
-				}
 					news[imag-1].className='iem '+'iemt '+'lef '+'bock'+' abs'+' trans';
 					news[imbg-1].className='iem '+'iemt '+'bock'+' trans';
 					news[imcg-1].className='iem '+'righ '+'bock'+' abs';
-					haha();
-	}
-
+					if (imag == 3) {
+							imag = 1;	
+						}
+					else{
+							imag +=1;				
+						}
+					if (imbg == 3) {
+							imbg = 1;	
+						}
+					else{					
+							imbg +=1;				
+						}
+					if (imcg == 3) {
+							imcg = 1;	
+						}
+					else{					
+							imcg +=1;				
+						}	
+				}
 		document.getElementById('prev').onclick=function(){
 					hehe();
-					for (var i = 0; i < news.length; i++) {
-							news[i].className ='iem';
-					}
-					news[imag-1].className='iem '+'iemt '+'righ '+'bock'+' abs'+' trans';
-					news[imbg-1].className='iem '+'iemt '+'bock'+' trans';
-					news[imcg-1].className='iem '+'lef '+'bock'+' abs';
-					haha();
+					news[imag-1].className='iem '+'righ '+'bock'+' abs'+' trans';
+					news[imbg-1].className='iem '+'iemt '+'lef '+'bock'+' abs';
+					news[imcg-1].className='iem '+'iemt '+'bock'+' trans';
+					if (imag == 1) {
+							imag = 3;	
+						}
+					else{
+							imag -=1;				
+						}
+					if (imbg == 1) {
+							imbg = 3;	
+						}
+					else{					
+							imbg -=1;				
+						}
+					if (imcg == 1) {
+							imcg = 3;	
+						}
+					else{					
+							imcg -=1;				
+						}	
 				}
 		function hehe(){
-			
-		}
-		function haha(){
-				if (imag == 3) {
-						imag = 1;	
-					}
-				else{
-						imag +=1;				
-					}
-				if (imbg == 3) {
-						imbg = 1;	
-					}
-				else{					
-						imbg +=1;				
-					}
-				if (imcg == 3) {
-						imcg = 1;	
-					}
-				else{					
-						imcg +=1;				
-					}	
-		}				
+				//next//
+				for (var i = 0; i < news.length; i++) {
+						news[i].className ='iem';
+				}
+					
+
+		}		
 	}
 
